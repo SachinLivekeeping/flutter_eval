6ae58a29fdd2c3255a6d8fc6e969f57dc83f8a4d
Sachin Bhankhar <sachinbhankhar2000@gmail.com>
2025-12-26 20:47:12 +0530

    working demo

--- examples/code_push_app/lib/main.dart
+++ examples/code_push_app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return HotSwapLoader(
       // Updates can be loaded from the network using http:// and https://,
       // or from local files/Flutter assets using file:// and asset:// respectively.
-      uri: 'asset://assets/hot_update.evc',
+      uri: 'https://storage.googleapis.com/flutter_eval/version_1.0.0.evc',
       strategy: HotSwapStrategy.immediate,
       child: MaterialApp(
         title: 'Flutter Demo',
